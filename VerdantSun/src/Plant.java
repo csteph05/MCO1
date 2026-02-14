1b67c0aab5514c7640f6fb49c6047734b9604331
csteph05 <charlescabrera697@gmail.com>
2026-02-14 23:48:30 +0800

    feat: Plant class

---
+++ VerdantSun/src/Plant.java
@@ -0,0 +1,33 @@
+public class Plant {
+
+    // Basic info about the plant
+    private final String name;
+    private float sellPrice;
+    private float seedPrice;
+    private int yield;
+
+    // growth stat
+    private int maxGrowth;
+    private int currentGrowth;
+
+    // Current state
+    private String preferredSoil;
+    private boolean isWatered;
+
+    // plant constructor
+    public Plant(String name, float sellPrice, float seedPrice,
+                 int yield, int maxGrowth, String preferredSoil){
+        this.name = name;
+        this.sellPrice = sellPrice;
+        this.seedPrice = seedPrice;
+        this.yield = yield;
+        this.maxGrowth = maxGrowth;
+        this.currentGrowth = 0; // specs: current Growth starts at 0
+        this.preferredSoil = preferredSoil;
+        this.isWatered = false; // specs: Defaults to false
+    }
+}
+
+
+
+
