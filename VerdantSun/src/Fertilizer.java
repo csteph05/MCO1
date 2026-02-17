96a42ac1547efb598c795403d0e497037405623f
csteph05 <charlescabrera697@gmail.com>
2026-02-17 17:11:50 +0800

    feat: added other classes

---
+++ VerdantSun/src/Fertilizer.java
@@ -0,0 +1,11 @@
+public class Fertilizer {
+    private String name;
+    private float price;
+    private int effectDays;
+
+    public Fertilizer(String name, float price, int effectDays){
+        this.name = name;
+        this.price = price;
+        this.effectDays = effectDays;
+    }
+}
---
+++ VerdantSun/src/WateringCan.java
@@ -0,0 +1,8 @@
+public class WateringCan {
+    private final int maxWtrLevel = 10;
+    private int currentWtrLevel;
+
+    public WateringCan(){
+        this.currentWtrLevel = maxWtrLevel;
+    }
+}
