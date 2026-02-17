8ee97ace1c96dda63ff63c07f726d972de515c31
csteph05 <charlescabrera697@gmail.com>
2026-02-17 23:10:33 +0800

    feat: Soil class

---
+++ VerdantSun/src/Soil.java
@@ -0,0 +1,13 @@
+public class Soil {
+    private String name;
+    private Plant plant;
+    private Fertilizer fertilizer;
+    private boolean isBlocked;
+
+    public Soil(String name){
+        this.name = name;
+        this.plant = null;
+        this.fertilizer = null;
+        this.isBlocked = false;
+    }
+}
