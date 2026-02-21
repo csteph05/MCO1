cdf75658425d187dff52367a224287ad54ef1211
csteph05 <charlescabrera697@gmail.com>
2026-02-22 00:50:40 +0800

    feat: get/set plant/soil classes

--- VerdantSun/src/Plant.java
+++ VerdantSun/src/Plant.java
@@ -14,9 +14,20 @@ public class Plant {
     private String preferredSoil;
     private boolean isWatered;
 
-    // plant constructor
+
+    /**
+     *  Constructor that creates a Plant instance with its name, sell/seed price, yield
+     *  maxGrowth, and it's preferredSoil.
+     *
+     * @param name is the name of the Plant
+     * @param sellPrice is the selling price per yield
+     * @param seedPrice is the cost of planting the seed
+     * @param yield is the number of crops produced
+     * @param maxGrowth
+     * @param preferredSoil is the preferred soil type of the plant
+     */
     public Plant(String name, float sellPrice, float seedPrice,
-                 int yield, int maxGrowth, String preferredSoil){
+                 int yield, int maxGrowth, String preferredSoil) {
         this.name = name;
         this.sellPrice = sellPrice;
         this.seedPrice = seedPrice;
@@ -27,8 +38,92 @@ public class Plant {
         this.isWatered = false; // specs: Defaults to false
     }
 
-    // todo: getters
-    // todo: methods, something for isWatered flag, grow etc.
+    /**
+     * Returns the name of the Plant.
+     *
+     * @return the player's name
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *  Returns the sellPrice of the Plant.
+     *
+     * @return the sellPrice
+     */
+
+    public float getSellPrice() {
+        return sellPrice;
+    }
+
+    /**
+     * Returns the seedPrice of the Plant.
+     *
+     * @return the seedPrice
+     */
+
+    public float getSeedPrice() {
+        return seedPrice;
+    }
+
+    /**
+     * Returns the yield of the Plant.
+     *
+     * @return the yield
+     */
+
+    public int getYield(){
+        return yield;
+    }
+
+    /**
+     * Returns the maximum growth stage of the Plant.
+     *
+     * @return the maxGrowth
+     */
+
+    public int getMaxGrowth(){
+        return maxGrowth;
+    }
+
+    /**
+     * Returns the current growth stage of the Plant.
+     *
+     * @return the currentGrowth
+     */
+    public int getCurrentGrowth(){
+        return currentGrowth;
+    }
+
+    /**
+     * Returns the preferredSoil type of the plant
+     *
+     * @return the preferredSoil type
+     */
+    public String getPreferredSoil(){
+        return preferredSoil;
+    }
+
+    /**
+     * Returns whether the Plant has been watered.
+     *
+     * @return true if watered, false is not watered
+     */
+    public boolean isWatered(){
+        return isWatered;
+    }
+
+    /**
+     * Sets the watered status of the Plant.
+     *
+     * @param watered True if plant is watered, false otherwise
+     */
+    public void setWatered(boolean watered){
+        this.isWatered = watered;
+    }
+
 }
 
 
--- VerdantSun/src/Soil.java
+++ VerdantSun/src/Soil.java
@@ -4,13 +4,84 @@ public class Soil {
     private Fertilizer fertilizer;
     private boolean isBlocked;
 
-    public Soil(String name){
+    /**
+     * Constructs a Soil tile with a given soil type.
+     * Initially, no plant or fertilizer is present and it is unblocked.
+     *
+     * @param name the type of the soil
+     */
+
+    public Soil(String name) {
         this.name = name;
         this.plant = null;
         this.fertilizer = null;
         this.isBlocked = false;
     }
 
-    // todo: getters
-    // todo: methods
+    /**
+     * Returns the type of soil
+     *
+     * @return soil type as a string
+     */
+
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * Returns the plant currently on the soil
+     *
+     * @return the plant object, or null if none
+     */
+    public Plant getPlant(){
+        return plant;
+    }
+
+    /**
+     * Returns the fertilizer applied on the soil
+     *
+     * @return the Fertilizer object, or null if none
+     */
+    public Fertilizer getFertilizer(){
+        return fertilizer;
+    }
+
+    /**
+     * Returns whether soil tile is blocked
+     *
+     * @return true if blocked, false otherwise
+     */
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    /**
+     * Sets a plant on the soil tile
+     *
+     * @param plant the Plant object to place on the soil
+     */
+    public void setPlant(Plant plant){
+        this.plant = plant;
+    }
+
+    /**
+     * Sets a fertilizer on the soil tile
+     *
+     * @param fertilizer the fertilizer to apply
+     */
+
+    public void setFertilizer(Fertilizer fertilizer){
+        this.fertilizer = fertilizer;
+    }
+
+    /**
+     *   Sets whether this soil tile is blocked or not
+     *
+     * @param blocked true if blocked, false to unblock
+     */
+    public void setBlocked(boolean blocked){
+        this.isBlocked = blocked;
+    }
+
+
 }
